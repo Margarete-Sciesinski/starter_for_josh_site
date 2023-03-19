5424f23b30313b4c4bde39173fddbcf9d3e64596
OakleyHands <101091084+OakleyHands@users.noreply.github.com>
2023-03-19 20:47:28 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer class="container-fluid text-center"> <!-- div for the main image that stretches to the edge of the page -->
+    
+    <div class="container"> <!-- div to hold content in middle of page -->
+        <div class="row"> <!-- gives access to BS columns -->
+
+            <div class="col-md-6">
+                <a class="navbar-brand" href="#">
+                    <span class="josh">JOSH</span>
+                    <span class="orange">WHITKIN</span></a>
+            </div>
+            <div class="col-md-6">
+                <p>Terms and Conditions</p>
+            </div>
+
+        </div> <!-- row -->
+    </div> <!-- container -->
+
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
