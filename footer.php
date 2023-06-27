bd43f5df9de31edd769869499d3d45d4bb257658
OakleyHands <101091084+OakleyHands@users.noreply.github.com>
2023-06-27 20:47:21 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,12 +4,11 @@
         <div class="row"> <!-- gives access to BS columns -->
 
             <div class="col-md-6">
-                <a class="navbar-brand" href="#">
-                    <span class="josh">JOSH</span>
-                    <span class="orange">WHITKIN</span></a>
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : endif; ?>
             </div>
+
             <div class="col-md-6">
-                <p>Terms and Conditions</p>
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : endif; ?>
             </div>
 
         </div> <!-- row -->
