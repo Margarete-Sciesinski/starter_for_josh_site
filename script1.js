76266be68a76da9513ac4639411405ff9a540572
OakleyHands <101091084+OakleyHands@users.noreply.github.com>
2023-06-27 20:48:17 +0800

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,26 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.timeline().from('.navbar', {scale:0, duration:2})
+.from('.navbar-brand', {opacity:0, scale:0, ease:"black"})
+.from('.tag', {opacity:0, y10:0, ease:"black"});
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+     trigger: '.aboutbg',
+     markers:true,
+     start:"bottom 90%", 
+     end:"top 30%",
+     scrub:3, 
+          }
+  
+});
+
+tl.from('.about-text',{
+ duration:1,
+      y:250,
+ opacity:0,
+}).from('.blocker',{
+ duration:1,
+ x:250,
+opacity:0,
+});
